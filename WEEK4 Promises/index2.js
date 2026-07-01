5198220a4c1d75de87cd10638425319669400d87
Megha482001 <sarkarmegha2020@gmail.com>
2026-07-01 23:43:57 +0530

    correct

--- WEEK4 Promises/index2.js
+++ WEEK4 Promises/index2.js
@@ -1,4 +1,9 @@
-const t = new Time();
+/*const t = new Time();
 console.log(t.getSecond());
 console.log(t.getMinute());
-console.log(t.getHour());
+console.log(t.getFullHour()); */
+
+const map = new Map();
+map.set('name', 'Alice');
+map.set('age', 30);
+console.log(map.get('name'));
